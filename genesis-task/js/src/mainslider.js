322e461e166ee1ff5de0b45888c91065273b1431
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-09-04 01:00:59 +0300

    one verification

--- genesis-task/js/src/mainslider.js
+++ genesis-task/js/src/mainslider.js
@@ -345,7 +345,8 @@ ready(() => {
 
     formSearchButton.onclick = (e) => { //SEARCHING GIRLS
         e.preventDefault();
-        const valueCity = document.querySelector('input[type=text]').getAttribute('value');
+        let valueCity = document.querySelector('input[type=text]').getAttribute('value');
+        if (!valueCity) {valueCity = ' '};
         const valueGender = document.querySelector('select').value;
         const valueAgeMin = document.getElementById('year-min').value;
         const valueAgeMax = document.getElementById('year-max').value;
