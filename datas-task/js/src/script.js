c0e64c343088281ea3003e62fd3fa77c4978223d
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-09-11 00:12:42 +0300

    trying scroll adding blocks

--- datas-task/js/src/script.js
+++ datas-task/js/src/script.js
@@ -12,8 +12,8 @@ ready(
    () => {
         const button = document.querySelector('button');
         button.addEventListener('click', () => {
-			let items; //value of our items, plased in items.json file
-			let listItems;
+			// let items; //value of our items, plased in items.json file
+			// let listItems;
 			fetch('/datas-task/items.json')
 				.then(  
 					function(response) {  
@@ -24,9 +24,9 @@ ready(
 						}
 						response.json().then(function(data) {  
 							// console.log(data);  
-							items = data;
-							console.log("items:", items);
-							listItems = items.map(item => {
+							// items = data;
+							// console.log("items:", items);
+							const listItems = data.map(item => {
 								return `
 										<li class="row-item">
 											<figure>
@@ -63,10 +63,82 @@ ready(
 					}  
 				)
 				.catch(function(err) {  
-					console.log('Fetch Error :-S', err);  
+					console.log('Fetch Error :', err);  
 				});
 
 
+		});
+
+		const switcher = document.querySelector('input[type=checkbox]');
+		// console.log(switcher.checked ? "CHECKED" : "NOT");
+		switcher.addEventListener("change", () => {
+			console.log(switcher.checked ? "CHECKED" : "NOT");
+			if(switcher.checked) {
+				window.onscroll = () => {
+					let scrollHeight=document.documentElement.scrollHeight;
+    				let clientHeight=document.documentElement.clientHeight;
+					let height=scrollHeight+clientHeight;
+					console.log('scrollHeight:', scrollHeight, "clientHeight :",clientHeight, "height: ",  height);
+					let scrollTopA = window.pageYOffset;
+					let scrollTopB = document.documentElement.scrollTop;
+					let scrollTopC = document.body.scrollTop;
+					console.log("scrollTopA",scrollTopA , "scrollTopB", scrollTopB, "scrollTopC", scrollTopC);
+
+					if(scrollTopA+clientHeight === scrollHeight) {
+						fetch('/datas-task/items.json')
+				.then(  
+					function(response) {  
+						if (response.status !== 200) {  
+							console.log('Looks like there was a problem. Status Code: ' +  
+							response.status);  
+							return;  
+						}
+						response.json().then(function(data) {  
+							// console.log(data);  
+							// items = data;
+							// console.log("items:", items);
+							const listItems = data.map(item => {
+								return `
+										<li class="row-item">
+											<figure>
+												<img src="img/${item.image}.jpg" alt="image">
+												<div class="stars">
+													<div class="star blackstar"></div>
+													<div class="star blackstar"></div>
+													<div class="star blackstar"></div>
+													<div class="star graystar"></div>
+													<div class="star graystar"></div>
+												</div>
+												<figcaption>
+													<h2>
+														${item.title}
+													</h2>
+													<p>
+														${item.paragraph}
+													</p>
+												</figcaption>
+											</figure>
+										</li>`
+							});
+							console.log(" new items: ", listItems);		
+
+							const ul = document.createElement('ul');
+							ul.classList.add('row');
+							listItems.forEach(el => {ul.innerHTML += el});
+							const wrapper = document.querySelector('.wrapper');
+							wrapper.appendChild(ul);
+							
+
+
+						});  
+					}  
+				)
+				.catch(function(err) {  
+					console.log('Fetch Error :', err);  
+				});
+					}
+				}
+			}
 		});
 
 
