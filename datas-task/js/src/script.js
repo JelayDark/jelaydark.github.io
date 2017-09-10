c072294030c505262d9dc94d7f4214fd801c01eb
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-09-10 20:42:35 +0300

    still trying

--- datas-task/js/src/script.js
+++ datas-task/js/src/script.js
@@ -11,41 +11,41 @@ function ready(fn) {
 ready(
    () => {
         const button = document.querySelector('button');
-        const loadURL = (url) => {
-            let oRequest = new XMLHttpRequest();
-            oRequest.open('GET', url, true);
-            // oRequest.setRequestHeader("User-Agent", navigator.userAgent);
-            oRequest.send();
-            oRequest.onreadystatechange = () => {
-              console.log("RESPONSETEXT", oRequest.responseText);
-              console.log("PARSED", JSON.parse(oRequest.responseText));
-              return JSON.parse(oRequest.responseText);
-            }
-        };
+        // const loadURL = (url) => {
+        //     let oRequest = new XMLHttpRequest();
+        //     oRequest.open('GET', url, true);
+        //     // oRequest.setRequestHeader("User-Agent", navigator.userAgent);
+        //     oRequest.send();
+        //     oRequest.onreadystatechange = () => {
+        //       console.log("RESPONSETEXT", oRequest.responseText);
+        //       console.log("PARSED", JSON.parse(oRequest.responseText));
+        //       return JSON.parse(oRequest.responseText);
+        //     }
+        // }; OLD
         button.addEventListener('click', () => {
-            const items = loadURL("/datas-task/items.json");
-            console.log("items: ",items);
+			let items;
+            // const items = loadURL("/datas-task/items.json");
+            // console.log("items: ",items);
+			fetch('/datas-task/items.json')
+				.then(  
+					function(response) {  
+						if (response.status !== 200) {  
+							console.log('Looks like there was a problem. Status Code: ' +  
+							response.status);  
+							return;  
+						}
+						response.json().then(function(data) {  
+							console.log(data);  
+							items = data;
+						});  
+					}  
+				)  
+				.catch(function(err) {  
+					console.log('Fetch Error :-S', err);  
+				});
         });
 
         
-        fetch('/datas-task/items.json')
-          	.then(  
-				function(response) {  
-				if (response.status !== 200) {  
-					console.log('Looks like there was a problem. Status Code: ' +  
-					response.status);  
-					return;  
-				}
-
-				// Examine the text in the response  
-				response.json().then(function(data) {  
-					console.log(data);  
-				});  
-				}  
-			)  
-			.catch(function(err) {  
-				console.log('Fetch Error :-S', err);  
-			});
 
 
 
