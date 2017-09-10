d877ac08a5e13f617b54f6352aa8dafd630d0dcd
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-09-11 00:46:13 +0300

    trash deleted

--- datas-task/js/src/script.js
+++ datas-task/js/src/script.js
@@ -1,6 +1,6 @@
 "use strict"
 
-function ready(fn) {
+const ready = fn => {
   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
     fn();
   } else {
@@ -8,136 +8,64 @@ function ready(fn) {
   }
 }
 
-ready(
-   () => {
-        const button = document.querySelector('button');
-        button.addEventListener('click', () => {
-			// let items; //value of our items, plased in items.json file
-			// let listItems;
-			fetch('/datas-task/items.json')
-				.then(  
-					function(response) {  
-						if (response.status !== 200) {  
-							console.log('Looks like there was a problem. Status Code: ' +  
-							response.status);  
-							return;  
-						}
-						response.json().then(function(data) {  
-							// console.log(data);  
-							// items = data;
-							// console.log("items:", items);
-							const listItems = data.map(item => {
-								return `
-										<li class="row-item">
-											<figure>
-												<img src="img/${item.image}.jpg" alt="image">
-												<div class="stars">
-													<div class="star blackstar"></div>
-													<div class="star blackstar"></div>
-													<div class="star blackstar"></div>
-													<div class="star graystar"></div>
-													<div class="star graystar"></div>
-												</div>
-												<figcaption>
-													<h2>
-														${item.title}
-													</h2>
-													<p>
-														${item.paragraph}
-													</p>
-												</figcaption>
-											</figure>
-										</li>`
-							});
-							console.log(" new items: ", listItems);		
-
-							const ul = document.createElement('ul');
-							ul.classList.add('row');
-							listItems.forEach(el => {ul.innerHTML += el});
-							const wrapper = document.querySelector('.wrapper');
-							wrapper.appendChild(ul);
-							
-
-
-						});  
-					}  
-				)
-				.catch(function(err) {  
-					console.log('Fetch Error :', err);  
-				});
-
-
-		});
-
-		const switcher = document.querySelector('input[type=checkbox]');
-		// console.log(switcher.checked ? "CHECKED" : "NOT");
-		switcher.addEventListener("change", () => {
-			if (!switcher.checked) {window.onscroll = null;}
-			else {
-				window.onscroll = () => {
-					if(document.documentElement.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight) <= 300) {
-						fetch('/datas-task/items.json')
-						.then(  
-							function(response) {  
-								if (response.status !== 200) {  
-									console.log('Looks like there was a problem. Status Code: ' +  
-									response.status);  
-									return;  
-								}
-								response.json().then(function(data) {  
-									// console.log(data);  
-									// items = data;
-									// console.log("items:", items);
-									const listItems = data.map(item => {
-										return `
-												<li class="row-item">
-													<figure>
-														<img src="img/${item.image}.jpg" alt="image">
-														<div class="stars">
-															<div class="star blackstar"></div>
-															<div class="star blackstar"></div>
-															<div class="star blackstar"></div>
-															<div class="star graystar"></div>
-															<div class="star graystar"></div>
-														</div>
-														<figcaption>
-															<h2>
-																${item.title}
-															</h2>
-															<p>
-																${item.paragraph}
-															</p>
-														</figcaption>
-													</figure>
-												</li>`
-									});
-									// console.log(" new items: ", listItems);		
-
-									const ul = document.createElement('ul');
-									ul.classList.add('row');
-									listItems.forEach(el => {ul.innerHTML += el});
-									const wrapper = document.querySelector('.wrapper');
-									wrapper.appendChild(ul);
-									
-
-
-								});  
-							}  
-						)
-						.catch(function(err) {  
-							console.log('Fetch Error :', err);  
-						});
+ready(() => {
+	const button = document.querySelector('button');
+	const addBlocks = () => { // function of adding blocks on page
+		fetch('/datas-task/items.json') //request to our items.json
+			.then(  
+				response => {  
+					if (response.status !== 200) {  
+						console.log('Looks like there was a problem. Status Code: ' + response.status);  
+						return;  
 					}
+					response.json().then(data => {  
+						const listItems = data.map(item => {
+							return `
+									<li class="row-item">
+										<figure>
+											<img src="img/${item.image}.jpg" alt="image">
+											<div class="stars">
+												<div class="star blackstar"></div>
+												<div class="star blackstar"></div>
+												<div class="star blackstar"></div>
+												<div class="star graystar"></div>
+												<div class="star graystar"></div>
+											</div>
+											<figcaption>
+												<h2>
+													${item.title}
+												</h2>
+												<p>
+													${item.paragraph}
+												</p>
+											</figcaption>
+										</figure>
+									</li>`
+						});
+						const ul = document.createElement('ul');
+						ul.classList.add('row');
+						listItems.forEach(el => {ul.innerHTML += el});
+						const wrapper = document.querySelector('.wrapper');
+						wrapper.appendChild(ul);
+					});  
+				}  
+			)
+			.catch(err => {  
+				console.log('Fetch Error:', err);  
+			});
+
+	}
+	button.addEventListener('click', addBlocks);
+
+	const switcher = document.querySelector('input[type=checkbox]');
+	switcher.addEventListener("change", () => {
+		if (!switcher.checked) {window.onscroll = null;}
+		else {
+			window.onscroll = () => {
+				if(document.documentElement.scrollHeight - (window.pageYOffset + document.documentElement.clientHeight) <= 300) { // 300 = padding-bottom of page
+					addBlocks();
 				}
 			}
-		});
-
-
-
-        
-
-
-
-
-    }
-);
+		}
+	});
+});
