5ed00f25aa74b75a457dabeef5138b5de8642370
Eugeniy Tulyakov <jelaydark@mail.ru>
2017-09-10 19:57:40 +0300

    still trying

--- datas-task/js/src/script.js
+++ datas-task/js/src/script.js
@@ -17,7 +17,7 @@ ready(
             // oRequest.setRequestHeader("User-Agent", navigator.userAgent);
             oRequest.send();
             console.log(oRequest);
-            return JSON.parse(oRequest.responseText);
+            return oRequest.responseText;
         };
         button.addEventListener('click', () => {
             loadURL("/datas-task/items.json");
